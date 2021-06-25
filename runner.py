54c6b60bd181cd606a12cfe524bbac1535d3ca44
mpmdika <mpmahardika.32@gmail.com>
2021-06-25 16:45:00 +0700

    Menghapus yang tidak penting

--- runner.py
+++ runner.py
@@ -40,18 +40,6 @@ def get_config():
 	parser.add_argument('--toy_data', action='store_true', help='use toy data for testing')
 	parser.add_argument('--iterative', action='store_true', help='run eclat in iterative method, else use the recusrive method')
 	
-	cuda_parser = parser.add_argument_group('Cuda settings')
-	cuda_parser.add_argument('--use_CUDA', action='store_true', help='run eclat with GPU to accelerate computation')
-	cuda_parser.add_argument('--block', type=int, default=16, help='block number for Cuda GPU acceleration')
-	cuda_parser.add_argument('--thread', type=int, default=16, help='thread number for Cuda GPU acceleration')
-	
-	plot_parser = parser.add_argument_group('Plot settings')
-	plot_parser.add_argument('--plot_block', action='store_true', help='Run all the values in the block list and plot runtime')
-	plot_parser.add_argument('--plot_thread', action='store_true', help='Run all the values in the thread list and plot runtime')
-	plot_parser.add_argument('--plot_support', action='store_true', help='Run all the values in the support list and plot runtime')
-	plot_parser.add_argument('--plot_support_gpu', action='store_true', help='Run all the values in the support list and plot runtime w/ gpu acceleration')
-	plot_parser.add_argument('--compare_gpu', action='store_true', help='Run all the values in the support list for both gpu and cpu version of eclat')
-	
 	io_parser = parser.add_argument_group('IO settings')
 	io_parser.add_argument('--input_path', type=str, default='./data/data.txt', help='input data path')
 	io_parser.add_argument('--output_path', type=str, default='./data/output.txt', help='output data path')
@@ -93,7 +81,7 @@ def read_data(data_path, skip_header=False, toy_data=False):
 #################
 # RUN ALGORITHM #
 #################
-def run_algorithm(data, mode, support, iterative, use_CUDA, block, thread):
+def run_algorithm(data, mode, support, iterative):
 	global text_area
 	iterative = True
 	if mode == 'apriori':
@@ -102,7 +90,7 @@ def run_algorithm(data, mode, support, iterative, use_CUDA, block, thread):
 		return result
 	elif mode == 'eclat':
 		viewProgress('Running Eclat algorithm with '+str(support)+' support and data shape: '+str(np.shape(data)))
-		result = eclat(data, support, iterative, use_CUDA, block, thread, text_area)
+		result = eclat(data, support, iterative, text_area)
 
 		return result
 	else:
@@ -125,7 +113,6 @@ def write_result(result, result_path):
 
 	viewProgress('Results have been successfully saved to: '+str(result_path))
 	return True
-
 def assert_at_most_one_is_true(*args):
     return sum(args) <= 1
 
@@ -138,6 +125,7 @@ def assert_at_most_one_is_true(*args):
 """
 def main(root):
 	global text_area
+	
 	args = get_config()
 	data = read_data(inFile.get(), toy_data=args.toy_data)
 	label_frame5 = LabelFrame(root, text="Hasil", width=50)
@@ -146,97 +134,19 @@ def main(root):
 	text_area = st.ScrolledText(label_frame5, width=60, height=17, font=("Times New Roman", 10))
 	text_area.grid(column=3, row=0, rowspan=8, padx=5)
 	text_area.configure(state='disabled')
-	
-	viewProgress(gpu_status)
-	
-	
-
-	#---argument error handling---#
-	if args.use_CUDA and alg.get() != 'eclat':
-		raise NotImplementedError()
-	assert assert_at_most_one_is_true(args.plot_support, args.plot_support_gpu, args.plot_block, args.plot_thread, args.compare_gpu)
-	if args.plot_support_gpu or args.compare_gpu or args.plot_block or args.plot_thread:
-		try: assert args.use_CUDA
-		except: raise ValueError('Must use Cuda for these experiments!')
-				
-	#---ploting mode handling---#
-	if not args.plot_support and not args.plot_support_gpu and not args.compare_gpu: 
-		experiment_list = [float(minsup.get())]
-	elif alg.get() == 'apriori': 
-		experiment_list = (0.35, 0.3, 0.25, 0.2)
-	elif alg.get() == 'eclat' and args.plot_support_gpu: 
-		experiment_list = (0.001, 0.0008, 0.0006, 0.0004, 0.0002)
-	elif alg.get() == 'eclat' and args.compare_gpu: 
-		experiment_list = (0.1, 0.08, 0.06, 0.04, 0.02, 0.01)
-	elif alg.get() == 'eclat': 
-		experiment_list = (0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05)
-	elif alg.get() == 'eclat' and args.plot_thread:
-		experiment_list = (16, 32, 64, 128, 256)
-	elif alg.get() == 'eclat' and args.plot_block:
-		experiment_list = (16, 32, 64, 128, 256)
-	else:
-		raise NotImplementedError()
-
-
-	duration = []
-	duration2 = []
-	for v in experiment_list:
-		viewProgress(85*'-')
-		start_time = time.time()
-		if args.plot_thread:
-			result = run_algorithm(data, alg.get(), float(minsup.get()), args.iterative, args.use_CUDA, block=args.block, thread=v)
-		elif args.plot_block:
-			result = run_algorithm(data, alg.get(), float(minsup.get()), args.iterative, args.use_CUDA, block=v, thread=args.thread)
-		else:
-			result = run_algorithm(data, alg.get(), v, args.iterative, args.use_CUDA, args.block, args.thread)
-		"""
-			result has len()==2, 
-			result[0]: the 3-dimensional Large K itemset,
-			result[1]: the dictionary storing the support of each itemset
-		"""
-		duration.append(time.time() - start_time)
-		viewProgress("Waktu Eksekusi: "+str(duration[-1])[:7])
-
 		
-		if args.compare_gpu:
-			start_time = time.time()
-			result = run_algorithm(data, alg.get(), v, args.iterative, False, args.block, args.thread)
-			duration2.append(time.time() - start_time)
-			viewProgress("Time duration w/o gpu: "+str(duration2[-1][:7]))
+	duration = []
+	start_time = time.time()
+	result = run_algorithm(data, alg.get(), float(minsup.get()), args.iterative)
+	"""
+	result has len()==2, 
+	result[0]: the 3-dimensional Large K itemset,
+	result[1]: the dictionary storing the support of each itemset
+	"""
+	duration.append(time.time() - start_time)
+	viewProgress("Execution Time: "+str(duration[-1])[:7])
 
-
-	if args.plot_support or args.plot_support_gpu or args.plot_block or args.plot_thread or args.compare_gpu:
-		fig = plt.figure()
-		ax = fig.add_subplot(111)
-		if args.compare_gpu:
-			line2, = plt.plot(experiment_list, duration2, marker='o', label='w/o gpu')
-			line1, = plt.plot(experiment_list, duration, marker='o', label='w/ gpu')
-			plt.legend(handles=[line1, line2])
-			for xy in zip(experiment_list, duration2):
-				ax.annotate('(%s, %.5s)' % xy, xy=xy, textcoords='data')
-		else:
-			plt.plot(experiment_list, duration, marker='o')
-		for xy in zip(experiment_list, duration):
-			ax.annotate('(%s, %.5s)' % xy, xy=xy, textcoords='data')
-		plt.ylabel('execution time (seconds)')
-		if args.plot_support or args.plot_support_gpu:
-			plt.xlabel('minimum support')
-			title = 'plot_' + alg.get() + '_support_vs_execution_time'
-		elif args.compare_gpu:
-			plt.xlabel('minimum support')
-			title = 'plot_compare_gpu'
-		elif args.plot_block:
-			plt.xlabel('block number')
-			title = 'plot_' + alg.get() + '_block_vs_execution_time'
-		elif args.plot_thread:
-			plt.xlabel('thread number')
-			title = 'plot_' + alg.get() + '_thread_vs_execution_time'
-		plt.title(alg.get())
-		plt.grid()
-		fig.savefig('./data/' + title + '.jpeg')
-	else:
-		done = write_result(result, outFile.get())
-
+	done = write_result(result, outFile.get())
 
 
 def inF(root):
@@ -329,7 +239,7 @@ def gui(root):
 	# Check GPU
 	import eclat
 	
-	gpu_status = str(text_area.get("1.0", END))[:-1]
+	
 
 	root.mainloop()
 
